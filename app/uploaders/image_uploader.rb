b6e5fc40eaeffea790c2a49d8487ac961d8a36b6
ddonche <dan@fatalfitness.com>
2018-06-09 15:34:35 +0000

    fixed image styling

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,9 +2,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
 
-  process resize_to_fit: [600, 600]
+  process resize_to_fill: [400, 300]
 
   version :thumb do
-    process resize_to_fill: [200,200]
+    process resize_to_fill: [200, 150]
   end
 end
