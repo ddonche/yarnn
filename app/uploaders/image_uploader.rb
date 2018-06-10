053f8096320bdf91207068bedb3943420d838d65
ddonche <dan@fatalfitness.com>
2018-06-10 19:59:47 +0000

    fixed image resizing issue

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,9 +2,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
 
-  process resize_to_fill: [400, 300]
-
-  version :thumb do
-    process resize_to_fill: [200, 150]
-  end
+  process :resize_to_limit => [500, nil]
 end
