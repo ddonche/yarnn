a3e8ddfe4c22a03345621feacb62376e51b6b933
ddonche <dan@fatalfitness.com>
2018-06-13 17:19:23 +0000

    added notifications to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+  before_action :set_notifications
+  
+  def set_notifications
+    if user_signed_in?
+      @notifications = Notification.where({ read: false, recipient_id: current_user.id })
+    end
+  end
   
   def after_sign_in_path_for(resource)
     feed_path
